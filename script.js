c5971af8cce9a07f439073cc6e356ca6df2f1beb
rmmassie <47798251+rmmassie@users.noreply.github.com>
2019-08-30 14:02:24 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints nonsense in the browser's dev tools console
+console.log('This isnt very useful');
